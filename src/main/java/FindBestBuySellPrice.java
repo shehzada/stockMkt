fbdee5d7e4fcd85f2470115f89947bbee0fffff1
shehzada <shezashraf@gmail.com>
2016-07-22 15:18:33 +0100

    added boxed version

--- src/main/java/FindBestBuySellPrice.java
+++ src/main/java/FindBestBuySellPrice.java
@@ -29,4 +29,23 @@ public class FindBestBuySellPrice {
         System.out.println(" time take " + endTime / 1000 + " ms");
         return results;
     }
+    
+    public Map<String,BestTrades> getBestTradePricesBoxedVersion(List<Double> tradeList) {
+        long startTime = System.nanoTime();
+        Map<String,BestTrades> results = new HashMap<>(1);
+        IntStream.range(0, tradeList.size())
+                .boxed()
+                .min(Comparator.comparing(tradeList::get))
+                .ifPresent(bestBuy -> {
+                    IntStream.range(bestBuy + 2, tradeList.size())
+                            .boxed()
+                            .max(Comparator.comparing(tradeList::get))
+                            .ifPresent(bestSell -> {
+                                results.put("bestBuySellPrices", (new BestTrades(tradeList.get(bestBuy), tradeList.get(bestSell))));
+                            });
+                } );
+        long endTime = System.nanoTime() - startTime;
+        System.out.println("Boxed version time take " + endTime / 1000 + " ms");
+        return results;
+    }
 }
